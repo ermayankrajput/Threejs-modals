47e84f1d4187f6909fb3b2037c4509fe9be79e95
prashant sirohi <er.mayankrajput95@gmail.com>
2024-05-25 12:09:34 +0530

    customer and vendor email

--- src/app/assign-quote/assign-quote.component.ts
+++ src/app/assign-quote/assign-quote.component.ts
@@ -26,8 +26,8 @@ export class AssignQuoteComponent {
 
   ngOnInit(): void {
     this.uuid = this.route.snapshot.paramMap.get('uuid');
-    console.log('Login status: '+this.authService.isUserLogin());
-    console.log("uuid: "+this.uuid);
+    // console.log('Login status: '+this.authService.isUserLogin());
+    // console.log("uuid: "+this.uuid);
     if(this.authService.isUserLogin() && this.authService.isVendor()){
       this.shareService.shareQuote(this.uuid,this.authService.currentUser().email).subscribe((response) => {
         this.resp = response;
@@ -43,11 +43,11 @@ export class AssignQuoteComponent {
     return this.validateLastNameMessage = this.last_name?.match(/^[a-zA-Z ]{2,30}$/) || !this.last_name ? {validation: true, message: ''} : {validation: false, message: "Enter valid name"};
   }
   validateEmail(){
-    return this.validateEmailMessage = this.email?.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) ? {validation: true, message: ''} : {validation: false, message: "Enter valid email"};
+    return this.validateEmailMessage = this.email?.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) ? {validation: true, message: ''} : {validation: false, message: "Enter valid email (输入有效的邮箱)"};
   }
 
   runValidation(){
-    return this.validateFirstName().validation && this.validateLastName().validation && this.validateEmail().validation
+    return this.validateEmail().validation
   }
 
   submitForm(){
@@ -60,7 +60,7 @@ export class AssignQuoteComponent {
           // console.log('this is vendor please redirect');
           this.shareService.shareQuote(this.uuid,this.email).subscribe((response) => {
             this.resp = response;
-            console.log(this.resp);
+            // console.log(this.resp);
             // console.log(this.resp.token);
             localStorage.setItem('user', JSON.stringify(this.resp.user));
             localStorage.setItem('token', this.resp.token);
